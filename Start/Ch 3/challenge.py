dbdc85a4598aca1373fdf5ec02f92df6fa865369
Gimhan Fernando <161706670+Gimhanfdo@users.noreply.github.com>
2024-10-26 23:55:59 +0000

    Chapter 3

--- Start/Ch 3/challenge.py
+++ Start/Ch 3/challenge.py
@@ -7,7 +7,6 @@
 
 from abc import ABC, abstractmethod
 
-
 class Asset(ABC):
     def __init__(self, price):
         self.price = price
@@ -16,13 +15,17 @@ class Asset(ABC):
     def __str__(self):
         pass
 
-
 class Stock(Asset):
     def __init__(self, ticker, price, company):
         super().__init__(price)
         self.company = company
         self.ticker = ticker
 
+    def __str__(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+    
+    def __lt__(self, other):
+        return self.price < other.price
 
 class Bond(Asset):
     def __init__(self, price, description, duration, yieldamt):
@@ -31,6 +34,11 @@ class Bond(Asset):
         self.duration = duration
         self.yieldamt = yieldamt
 
+    def __str__(self):
+        return f"{self.description}: {self.duration}yr : ${self.price} : {self.yieldamt}%"
+
+    def __lt__(self, other):
+        return self.yieldamt < other.yieldamt
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
 stocks = [
